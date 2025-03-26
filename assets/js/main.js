89070bd23bf9dc87c65993bd959a8fa8be50c5cb
konoec <kono.caec@gmail.com>
2025-03-26 18:39:43 -0500

    Selección en el nav inferior

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,6 +198,22 @@ window.addEventListener("scroll", () => {
 ===================================================== */
 
 // Each bottom navigation menu items active on page scroll.
+window.addEventListener("scroll", () => {
+    const navMenuSections = document.querySelectorAll(".nav-menu-section");
+    const scrollY = window.pageYOffset;
+
+    navMenuSections.forEach((navMenuSection) => {
+        let sectionHeight = navMenuSection.offsetHeight;
+        let sectionTop = navMenuSection.offsetTop - 50;
+        let id = navMenuSection.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".bottom-nav .menu li a[href*=" + id + "]").classList.add("current");
+        }else {
+            document.querySelector(".bottom-nav .menu li a[href*=" + id + "]").classList.remove("current");
+        }
+    });
+})
 
 // Javascript to show bottom navigation menu on home(page load).
 
