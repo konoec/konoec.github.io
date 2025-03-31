6b576c3980cb512dc0d6a2f83c9985cff8a70fd1
konoec <kono.caec@gmail.com>
2025-03-30 22:42:17 -0500

    Animaciones implementadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -357,5 +357,21 @@ document.addEventListener("DOMContentLoaded", () => {
 ===================================================== */
 
 // Common reveal options to create reveal animations.
+ScrollReveal({
+    // reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
 
 // Target elements and specify options to create reveal animations.
+ScrollReveal().reveal('.avatar-img', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.avatar-info, .section-title', { delay: 300, origin: 'top' });
+ScrollReveal().reveal('.home-social, .home-scroll-btn, .copy-right', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.about-img', { delay: 700, origin: 'top' });
+ScrollReveal().reveal('.about-info, .konoec-footer .konoec-logo', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.pro-card, .about-buttons .konoec-main-btn, .resume-tabs .tab-btn, .portfolio-tabs .tab-btn', { delay: 500, origin: 'right', interval: 200});
+ScrollReveal().reveal('#resume .section-content', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.strength-card, .portfolio-card, .contact-item, .contact-social-links li, .footer-menu .menu-item', { delay: 300, origin: 'bottom',interval: 300 });
+ScrollReveal().reveal('.konoec-client-swiper, .contact-form-body', { delay: 700, origin: 'right' });
+ScrollReveal().reveal('.contact-info h3', { delay: 100, origin: 'bottom', interval: 300 });
